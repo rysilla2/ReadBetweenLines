ca53f46d3352b62b3059c355968fe305d2edc47e
Renato Tan III <renato.tan@uap.asia>
2023-10-08 19:31:19 +0800

    Changed naming style of classes and organized css file

--- features/welcome/welcome-screen-script.js
+++ features/welcome/welcome-screen-script.js
@@ -1,10 +1,16 @@
 /* implementation to get the Age of the user based on the selected birthdate from the calendar */
 function calculateAge() {
-  const selectedDate = new Date(document.getElementById("Birthday").value);
+  const selectedDate = new Date(document.getElementById("birthday").value);
   const currentDate = new Date();
   const age = Math.floor(
     (currentDate - selectedDate) / (365.25 * 24 * 60 * 60 * 1000)
   );
+  if (age < 4) {
+    alert('This is temporary but this indicates your age is too low')
+  }
+}
+
+const getDifficulty = (age) => {
 
   let difficulty = "";
 
@@ -16,9 +22,10 @@ function calculateAge() {
   } else {
     difficulty = "Philosopher";
   }
-
-  document.getElementById("age").textContent = `Age: ${age} years`;
-  document.getElementById("difficulty").textContent = `You are a ${difficulty}`;
 }
 
-flatpickr("#Birthday", {});
+flatpickr("#birthday", {});
+
+// const onStart = () => {
+//   if 
+// }
