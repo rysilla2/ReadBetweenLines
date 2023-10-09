adb12d2519516be3b5ebcf752c0fff4996c12e20
Renato Tan III <renato.tan@uap.asia>
2023-10-09 14:38:11 +0800

    Add button functionality for difficulty buttons

--- features/landing/main-landing-screen-script.js
+++ features/landing/main-landing-screen-script.js
@@ -1,75 +1,119 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const difficultyButtons = document.querySelectorAll(".difficulty-rectangle");
-    const categoryForm = document.getElementById("categoryForm");
-    const difficultyLevelInput = document.getElementById("difficultyLevel");
-    const categoryInput = document.getElementById("categoryInput");
-    const randomizeButton = document.querySelector(".btn-randomize");
-    const categoryInputField = document.getElementById("category");
+  const difficultyButtons = document.querySelectorAll(".difficulty-rectangle");
+  const categoryForm = document.getElementById("categoryForm");
+  const difficultyLevelInput = document.getElementById("difficultyLevel");
+  const categoryInput = document.getElementById("categoryInput");
+  const randomizeButton = document.querySelector(".btn-randomize");
+  const categoryInputField = document.getElementById("category");
 
-    // Function to handle button click
-    function handleButtonClick(event) {
-        // Remove the 'active' class from all buttons
-        difficultyButtons.forEach((button) => {
-            button.classList.remove("active");
-        });
-
-        // Add the 'active' class to the clicked button
-        event.currentTarget.classList.add("active");
-    }
-
-    // Function to generate a random category
-    function getRandomCategory() {
-        const categories = ["country", "animal", "food", "color", "marvel hero"];
-        const randomIndex = Math.floor(Math.random() * categories.length);
-        return categories[randomIndex];
-    }
-
-    // Function to populate the input field with a random category
-    function populateRandomCategory() {
-        const randomCategory = getRandomCategory();
-        categoryInputField.value = randomCategory;
-    }
-
-    // Attach click event listeners to each difficulty button
+  // Function to handle button click
+  function handleButtonClick(event) {
+    // Remove the 'active' class from all buttons
     difficultyButtons.forEach((button) => {
-        button.addEventListener("click", handleButtonClick);
+      button.classList.remove("active");
     });
 
-    // Attach click event listener to the Randomize button
-    randomizeButton.addEventListener("click", function (event) {
-        event.preventDefault(); // Prevent form submission
-        populateRandomCategory();
-    });
+    // Add the 'active' class to the clicked button
+    event.currentTarget.classList.add("active");
+  }
 
-    // Add a submit event listener to the form
-    categoryForm.addEventListener("submit", function (event) {
-        // Prevent the default form submission
-        event.preventDefault();
+  // Function to generate a random category
+  function getRandomCategory() {
+    const categories = ["country", "animal", "food", "color", "marvel hero"];
+    const randomIndex = Math.floor(Math.random() * categories.length);
+    return categories[randomIndex];
+  }
 
-        // Get the selected difficulty level
-        const selectedDifficulty = document.querySelector(".difficulty-rectangle.active");
-        if (selectedDifficulty) {
-            difficultyLevelInput.value = selectedDifficulty.id; // Set the difficulty level value
-        }
+  // Function to populate the input field with a random category
+  function populateRandomCategory() {
+    const randomCategory = getRandomCategory();
+    categoryInputField.value = randomCategory;
+  }
 
-        // Submit the form
-        categoryForm.submit();
-    });
+  // Attach click event listeners to each difficulty button
+  difficultyButtons.forEach((button) => {
+    button.addEventListener("click", handleButtonClick);
+  });
 
-    
+  // Attach click event listener to the Randomize button
+  randomizeButton.addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent form submission
+    populateRandomCategory();
+  });
+
+  // Add a submit event listener to the form
+  categoryForm.addEventListener("submit", function (event) {
+    // Prevent the default form submission
+    event.preventDefault();
+
+    // Get the selected difficulty level
+    const selectedDifficulty = document.querySelector(
+      ".difficulty-rectangle.active"
+    );
+    if (selectedDifficulty) {
+      difficultyLevelInput.value = selectedDifficulty.id; // Set the difficulty level value
+    }
+
+    // Submit the form
+    categoryForm.submit();
+  });
 });
 
-const getDifficulty = () => { sessionStorage.getItem("userInformation");
-    let difficulty = "";
-    let userInformation = sessionStorage.getItem("userInformation"); 
-    let age = JSON.parse(userInformation).age;
-    
-    /* Difficulty Based on user's age */
-    if (age <= 8) {
-      difficulty = "Ludus";
-    } else if (age <= 15) {
-      difficulty = "Rhetor";
-    } else {
-      difficulty = "Philosopher";
-    }
-  };
+const getDifficulty = () => {
+  sessionStorage.getItem("userInformation");
+  let difficulty = "";
+  let userInformation = sessionStorage.getItem("userInformation");
+  let age = JSON.parse(userInformation).age;
+
+  /* Difficulty Based on user's age */
+  if (age <= 8) {
+    difficulty = "Ludus";
+  } else if (age <= 15) {
+    difficulty = "Rhetor";
+  } else {
+    difficulty = "Philosopher";
+  }
+};
+
+document.getElementById("ludus").addEventListener("click", () => {
+  console.log('clicked');
+  sessionStorage.setItem(
+    "roundDetails",
+    JSON.stringify({
+      difficulty: "ludus",
+      time: 40,
+      turns: 10,
+      rounds: 1,
+    })
+  );
+  goToPuzzlePage();
+});
+document.getElementById("rhetor").addEventListener("click", () => {
+  sessionStorage.setItem(
+    "roundDetails",
+    JSON.stringify({
+      difficulty: "rhetor",
+      time: 20,
+      turns: 7,
+      rounds: 3,
+    })
+  );
+  goToPuzzlePage();
+});
+document.getElementById("philosopher").addEventListener("click", () => {
+  sessionStorage.setItem(
+    "roundDetails",
+    JSON.stringify({
+      difficulty: "philosopher",
+      time: 10,
+      turns: 5,
+      rounds: 5,
+    })
+  );
+  goToPuzzlePage();
+});
+
+const goToPuzzlePage = () => {
+  document.location.href =
+    document.location.origin + "/features/puzzle/puzzle-screen.html";
+};
