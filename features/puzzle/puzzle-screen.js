d684c708d6abee6871f57197334d288360556099
jggaza <115634017+jggaza@users.noreply.github.com>
2023-10-09 15:31:33 +0800

    made function call timestop for store details function

--- features/puzzle/puzzle-screen.js
+++ features/puzzle/puzzle-screen.js
@@ -193,45 +193,47 @@ function hideControlPanel(isHidden) {
 
 // this is for storing the match details
 function storeDetails(category, word, winner, timestop) {
-
+ // this a function call for stop the timer stopTimer()
     const details = {
         category: category,
         word: word,
         winner: winner,
         //  duration:duration,
-        timestop: timestop, // to record the duration or what time did it stop
+        timestop: stopTimer(), // to record the duration or what time did it stop
     };
     // this is for storing it in the session storage
     sessionStorage.setItem('storeDetails', JSON.stringify(details));
 
 }
 
+let time = 0; //for setting the initial time
 // this is the stopwatch to track the duration
 window.addEventListener("load", function () {
-    const stopwatch = this.document.getElementById("stopwatch");
 
-    let time = 0; //for setting the initial time
+
 
     function incrementTime() {
         time++;
-
-        stopwatch.textContent = ("0" + Math.trunc(time / 60)).slice(-2) + ":" +
-            ("0" + (time % 60)).slice(-2);
     }
 
     incrementTime();//start the timer 
-    this.setInterval(incrementTime, 1000);
+    this.setInterval(incrementTime, 1);
 
     //need final round to end to stop the watch 
     // for recording the duration
 
     //testing this is for checking if the current round is the final round
-    if (currentRound === originalRounds) {
-        clearInterval(incrementTime);// to stop the timer
-    }
+    //if (currentRound === originalRounds) {
+    //    clearInterval(incrementTime);// to stop the timer
+    //}
 
 })
 
+function stopTimer(){ //stopTimer
+    clearInterval(interval);
+    const elapsedTime = time;
+    return elapsedTime;
+}
 
 //saves the word into local storage.
 function saveWord(word) {
